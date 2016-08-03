d923400cca4281b9577aefd3923a333c7dd4f9c3
Henry Ho <hxl2000@gmail.com>
2016-08-04 02:14:05 +0800

    unset a function

---
+++ func02.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Shell Script Name: func02.sh
+#Describe:Unset a function
+#Author:Henry Ho
+#Version:1.0.0
+#Date:8-3-2016
+
+function()
+{
+	echo "Hello world!"
+}
+
+unset function
+function
