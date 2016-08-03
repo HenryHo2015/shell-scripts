824b14c4731b36a0398f0cc649844b94f31ee983
Henry Ho <hxl2000@gmail.com>
2016-08-04 02:09:43 +0800

    Define a function in shell script

---
+++ func01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Shell Script Name: func01.sh
+#Describe:Define a function in shell script
+#Author:Henry Ho
+#Version:1.0.0
+#Date:8-3-2016
+
+myfunction()	#Define a function
+{
+	echo " Hello world!"
+}
+
+myfunction	#Call function
