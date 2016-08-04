23ba0056f4c4517999ee3120df5cbbc85f9f1275
Henry Ho <hxl2000@gmail.com>
2016-08-05 01:35:14 +0800

    using local command in shell script

---
+++ 03/use_local_var.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Shell Script Name: Hello world!
+#Describe:Print Hello world string on screen
+#Author:Henry Ho
+#Version:1.0.0
+#Date:8-3-2016
+
+emc()
+{
+	local name
+	name=apple
+	echo "OS name: $name"
+}
+
+emc
+
+echo $name
