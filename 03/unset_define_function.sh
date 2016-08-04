528a00a5248f4538d3825e396c33b09aa42993da
Henry Ho <hxl2000@gmail.com>
2016-08-05 01:15:36 +0800

    ---+++

---
+++ 03/unset_define_function.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Shell Script Name: Hello world!
+#Describe:Print Hello world string on screen
+#Author:Henry Ho
+#Version:1.0.0
+#Date:8-3-2016
+
+myfunction()
+{
+	echo "What a nice day today!"
+}
+
+myfunction
+
+unset myfunction
+
+myfunction
+
+myfunction()
+{
+	echo "What a nice day today!"
+}
+
+myfunction
+
