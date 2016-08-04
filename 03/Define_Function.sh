ee14b74775b18409c41bfc6ee3f478a9e2620e05
Henry Ho <hxl2000@gmail.com>
2016-08-05 01:06:06 +0800

    Define a function in a shell script.

---
+++ 03/Define_Function.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Shell Script Name: Define_Function.sh
+#Describe: Define a function in shell script
+#Author:Henry Ho
+#Version:1.0.0
+#Date:8-4-2016
+
+#function ()
+#{
+#command...
+#}
+
+myfunction()		#Define a function
+{
+echo "What a fine day today!"
+}
+
+myfunction	#Call function
+
+
