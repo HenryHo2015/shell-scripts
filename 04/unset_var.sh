9bc316077a7afe2691059818b9ece2349eef9fe3
Henry Ho <hxl2000@gmail.com>
2016-08-06 00:59:03 +0800

    Unset a variable in shell script

---
+++ 04/unset_var.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Shell Script Name: Hello world!
+#Describe:Print Hello world string on screen
+#Author:Henry Ho
+#Version:1.0.0
+#Date:8-3-2016
+
+myfunction()
+{
+bigboy=man
+echo "I am a $bigboy"
+
+unset bigboy
+echo "I am a $bigboy"
+
+}
+
+myfunction
