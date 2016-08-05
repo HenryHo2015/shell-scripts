ba4bb34198c3a1dcbae283cf8839f37d01ff637f
Henry Ho <hxl2000@gmail.com>
2016-08-06 00:56:39 +0800

    Define a variable in shell script

---
+++ 04/define_shell_vars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Shell Script Name: define_shell_vars.sh
+#Describe:Define shell vars
+#Author:Henry Ho
+#Version:1.0.0
+#Date:8-5-2016
+
+myfunction()
+{
+	bigboy=man
+	echo "I am a $bigboy"
+}
+
+myfunction
+
+
