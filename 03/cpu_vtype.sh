05ecc106c2ee8a8311220fddde72fc613e32773d
HenryHo2016 <hxl2000@gmail.com>
2016-08-13 12:13:14 -0600

    Update cpu_vtype.sh

--- 03/cpu_vtype.sh
+++ 03/cpu_vtype.sh
@@ -3,11 +3,12 @@
 
 cpu_support_vtype()
 {
-vtype=`lscpu |grep "Virtualization type"|cut -d: -f2`
-echo $vtype
-return 0
+  vtype=`lscpu |grep "Virtualization type"|cut -d: -f2`
+  echo $vtype
+  return 0
 }
 
-cpu_support_vtype
-unset cpu_support_vtype
-unset vtype
+cpu_support_vtype #Call defined function
+
+unset cpu_support_vtype   #Destroy function cpu_support_vtype()
+unset vtype #Destroy variable vtype
