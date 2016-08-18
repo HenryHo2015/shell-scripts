45fcd86bba3cbad1ad858dac8351297d46f808d7
Henry <hxl2000@gmail.com>
2016-08-18 11:51:07 -0600

    Update

---
+++ 03/check_root.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Judging root user or not
+
+check_root()
+{
+if [ $UID -ne 0 ]; then
+        echo "Please run as root"
+	local check_root = true
+
+else
+        echo "Root User"
+	local check_root = false
+fi
+}
+
+check_root
