e16f87961a23d42dbb5b2c517a2c593edd40d821
Henry <hxl2000@gmail.com>
2016-08-19 23:32:37 -0600

    if exercise two

---
+++ if_ex2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#if [ condition ]
+
+if test -f "$1" 
+then
+	echo "$1 is an ordinary file"
+else
+	echo "$1 is not ordinary file"
+fi
+
+if [ -d $1 ]; 
+then
+	echo "$1 is a directory"
+else
+	echo "$1 is not a directory"
+fi
+
+
+
