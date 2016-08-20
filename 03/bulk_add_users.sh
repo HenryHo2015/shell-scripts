7238bca29d36531df75424fb27a63731e2be1e1f
Henry <hxl2000@gmail.com>
2016-08-19 22:43:59 -0600

    +>/dev/null 2>&1

--- 03/bulk_add_users.sh
+++ 03/bulk_add_users.sh
@@ -6,9 +6,9 @@ read -p "Please enter number you want to add: " num
 add_user()
 {
 	read -p "Enter user name:" username
-	useradd $username
+	useradd $username >/dev/null 2>&1
 	read -p "Enter user password:" password
-	echo $password | passwd --stdin $username 
+	echo $password | passwd --stdin $username >/dev/null 2>&1
 }
 
 for i in $(seq 1 $num)
